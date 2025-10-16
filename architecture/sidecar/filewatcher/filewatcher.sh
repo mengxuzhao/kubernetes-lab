4334ceab9172e44f07e6f64e5460656de0f73c6e
Meng Xu <meng.xu.zhao@gmail.com>
2025-10-16 14:56:19 +1100

    Delete architecture/sidecar directory

--- architecture/sidecar/filewatcher/filewatcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WATCHDIR="/data/"
-inotifywait -m -q -e close_write --format %f ${WATCHDIR} | while IFS= read -r file
-do
-    aws sns publish --topic-arn "${SNS_TOPIC_ARN}" --message "A new file is uploaded - ${file}"
-done
